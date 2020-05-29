97bf5095bdb07eac0d27f9260723a303a5081a9d
Errors4Days <justin.j.lee14@gmail.com>
2020-05-29 16:47:22 -0400

    Functioning email client on webpage to preset email account.

--- emailform.php
+++ emailform.php
@@ -1,17 +1,47 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
     
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/SMTP.php';
+    require 'PHPMailer/Exception.php';
+        
     if(isset($_POST["submit"])){
         $name = $_POST["name"];
         $email = $_POST["email"];
         $message = $_POST["message"];
         $subject = $_POST["subject"];
-    }
-    $mailTo = "woddergood4u@gmail.com";
-    $headers = "From: ".$email;
-    $txt = "You have received an email from ".$name.".\n\n".$message;
-    mail($email, $subject, $message);
-    
+        
+        $mail = new PHPMailer(true);
+        $body = "You have received an email from ".$name.".\n\n".$message;
+        
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'woddergood4u@gmail.com';
+            $mail->Password = 'tidepodsgood123';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
 
-    header("Location: form.html?mailsend");
-    
+            //Recipients
+            $mail->setFrom('woddergood4u@gmail.com', 'Mailer');
+            $mail->addAddress('woddergood4u@gmail.com');
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+
+            $mail->send();
+            echo '<script>alert("Email sent successfully!")</script>'; 
+            header("Location: form.html?mailsend");
+        } 
+        catch (Exception $e){
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
 ?>
